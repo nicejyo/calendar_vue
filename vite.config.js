85c85e3ac6b790288baa372b5ed1c4116aad724f
jyo-room-admin <nicejyo33@gmail.com>
2025-01-21 19:19:18 +0900

    cloudtype api 호출 url 정의

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,13 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
+// http://localhost:8080
+// https://app.cloudtype.io/@nicejyo/calendar:main/calendar-boot
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://port-0-calendar-api-m6647c5e231f83ec.sel4.cloudtype.app',
       },
     },
   },
